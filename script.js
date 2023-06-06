d3d35f8893dda55ca4de94d677578cb27d7a7098
muskaanjain017 <78611316+muskaanjain017@users.noreply.github.com>
2023-06-06 23:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,9 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-function t(teams) {
-	return teams;
-}
-const team = t(players);
-const team1 = players;
-const cap1 = person;
+const team = players;
+const team1 = [...players];
+const cap1 = { ...person};
 cap1.name = "Dhoni";
 window.players = players;
 window.person = person;
